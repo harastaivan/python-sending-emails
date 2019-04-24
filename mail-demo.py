ce97613025bfb261551e9a99bbe58f4187f7d90d
Ivan Harasta <harasta.ivan@gmail.com>
2019-04-24 12:35:42 +0200

    Image as attachment

--- mail-demo.py
+++ mail-demo.py
@@ -1,6 +1,7 @@
 
 import os
 import smtplib
+import imghdr
 from email.message import EmailMessage
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
@@ -10,9 +11,18 @@ msg = EmailMessage()
 msg['Subject'] = 'Grab dinner this weekend?'
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = 'harasta.ivan@gmail.com'
-msg.set_content('How about dinner at 6pm this Saturday?')
+msg.set_content('Image attached...')
+
+files = ['JF.jpg']
+
+for file in files:
+    with open(file, 'rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+
+    msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
     smtp.send_message(msg)
